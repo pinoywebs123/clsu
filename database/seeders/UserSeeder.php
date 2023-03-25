c12642470fb7abd9bb2ad3465f17dc4b4afbbf35
Morls <moravel752@gmail.com>
2023-03-25 21:40:06 +0800

    update seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class UserSeeder extends Seeder
 {
@@ -15,6 +17,16 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        
+        $user = new User;
+        $user->first_name   = 'Admin';    
+        $user->last_name    = 'User';
+        $user->password     = bcrypt('admin123');
+        $user->email        = 'admin@gmail.com';
+        $user->status_id    = 1;
+        $user->department_id = 0;
+        $user->save();
+
+        $user->assignRole('admin');
     }
 }
