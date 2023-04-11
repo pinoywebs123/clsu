8e01476f58117d1c1db7925a62746acd24ceb353
Morls <moravel752@gmail.com>
2023-04-11 14:05:19 +0800

    fixed logo dashboard rotate

--- resources/views/department/request.blade.php
+++ resources/views/department/request.blade.php
@@ -35,7 +35,7 @@
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
+                <div class="sidebar-brand-icon">
                     <img src="{{URL::to('/img/clsu_logo.png')}}" width="50px">
                 </div>
                  <div class="sidebar-brand-text mx-3">
